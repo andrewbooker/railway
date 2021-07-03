9b7315af1fa4111c80246db21631907c114c836e
Andrew Booker <improvizone@gmail.com>
2021-07-03 22:38:34 +0100

    factoring out points selection and testing loop with siding

--- lib/navigator.py
+++ lib/navigator.py
@@ -7,6 +7,7 @@ class Journey():
         self.direction = "forward"
         self.history = []
         self._at(self.layout["sections"][0])
+        self.selectPoints = lambda: "left" if (randint(0, 1) > 0) else "right"
 
     def _find(self, id):
         if "points" in self.layout:
@@ -23,7 +24,7 @@ class Journey():
         self.history.append((s["id"], self.direction))
         print("now on", s["name"])
 
-    def _changeDirection(self):
+    def changeDirection(self):
         self.direction = "forward" if self.direction == "reverse" else "reverse"
         self.history.append((self.section["id"], self.direction))
 
@@ -32,11 +33,12 @@ class Journey():
         if "next" not in self.section:
             if self.section["id"][0] == "p":
                 if self.direction == "forward":
-                    choice = self.section["param"] if "param" in self.section else ("left" if (randint(0, 1) > 0) else "right")
+                    choice = self.section["param"] if "param" in self.section else self.selectPoints()
                     print("choosing", choice)
                     self._at(self._find(self.section[choice]["id"]))
                 else:
-                    print("points expected to be", "left" if self.section["left"]["id"] == self.history[-2] else "right")
+                    expectedPoints = "left" if self.section["left"]["id"] == self.history[-2] else "right"
+                    print("points expected to be", expectedPoints)
                     for s in self.layout["sections"]:
                         if "forward" in s["next"] and s["next"]["forward"]["id"] == self.section["id"]:
                             self._at(s)
@@ -45,5 +47,5 @@ class Journey():
             if self.direction in options:
                 self._at(self._find(options[self.direction]["id"]))
             else:
-                self._changeDirection()
+                self.changeDirection()
                 print("changing direction to", self.direction)
--- lib/test_navigator.py
+++ lib/test_navigator.py
@@ -1,19 +1,116 @@
 
 from navigator import Journey
 
+straightLine = "{\"sections\":[{\"id\":\"s1\",\"name\":\"shuttle\",\"next\":{}}]}"
+
 def test_straight_line_track_at_start():
-    journey = Journey("{\"sections\":[{\"id\":\"s1\",\"name\":\"shuttle\",\"next\":{}}]}")
+    journey = Journey(straightLine)
     assert journey.history == [("s1", "forward")]
-    
+
 def test_straight_line_track_after_one_move():
-    journey = Journey("{\"sections\":[{\"id\":\"s1\",\"name\":\"shuttle\",\"next\":{}}]}")
+    journey = Journey(straightLine)
     journey.nextStage()
     assert journey.history == [("s1", "forward"),("s1", "reverse")]
-    
+
 def test_straight_line_track_after_multiple_moves():
-    journey = Journey("{\"sections\":[{\"id\":\"s1\",\"name\":\"shuttle\",\"next\":{}}]}")
+    journey = Journey(straightLine)
     journey.nextStage()
     journey.nextStage()
     journey.nextStage()
     journey.nextStage()
     assert journey.history == [("s1", "forward"),("s1", "reverse"),("s1", "forward"),("s1", "reverse"),("s1", "forward")]
+
+
+loop = "{\"sections\":[{\"id\":\"s01\",\"name\":\"loop\",\"next\":{\"forward\":{\"id\":\"s01\"},\"reverse\":{\"id\":\"s01\"}}}]}"
+
+def test_loop_at_start():
+    journey = Journey(loop)
+    assert journey.history == [("s01", "forward")]
+
+def test_loop_after_multiple_moves():
+    journey = Journey(loop)
+    journey.nextStage()
+    journey.nextStage()
+    assert journey.history == [("s01", "forward"),("s01", "forward"),("s01", "forward")]
+
+def test_loop_after_change_of_direction():
+    journey = Journey(loop)
+    journey.nextStage()
+    journey.changeDirection()
+    journey.nextStage()
+    journey.nextStage()
+    assert journey.history == [("s01", "forward"),("s01", "forward"),("s01", "reverse"),("s01", "reverse"),("s01", "reverse")]
+
+
+loopWithSiding = """
+{"sections":[
+    {
+        "id": "s01",
+        "name": "main loop",
+        "speed": "express",
+        "length": 10.6,
+        "next": {
+            "forward": {
+                "id": "p01"
+            },
+            "reverse": {
+                "id": "p01",
+                "param": "right"
+            }
+        }
+    },
+    {
+        "id": "s02",
+        "name": "branch siding",
+        "speed": "crawl",
+        "length": 1.5,
+        "next": {
+            "reverse": {
+                "id": "p01",
+                "param": "left"
+            }
+        }
+    }
+],
+"points": [
+    {
+        "id": "p01",
+        "name": "branch siding points",
+        "left": {
+            "id": "s02"
+        },
+        "right": {
+            "id": "s01"
+        }
+    }
+]}
+"""
+
+def test_loop_with_siding_points_right():
+    journey = Journey(loopWithSiding)
+    journey.selectPoints = lambda: "right"
+    journey.nextStage()
+    journey.nextStage()
+    journey.nextStage()
+    assert journey.history == [("s01", "forward"), ("p01", "forward"), ("s01", "forward"), ("p01", "forward")]
+
+def test_loop_with_siding_points_left():
+    journey = Journey(loopWithSiding)
+    journey.selectPoints = lambda: "left"
+    journey.nextStage()
+    journey.nextStage()
+    journey.nextStage()
+    journey.nextStage()
+    journey.nextStage()
+    journey.nextStage()
+    journey.nextStage()
+    assert journey.history == [
+        ("s01", "forward"),
+        ("p01", "forward"),
+        ("s02", "forward"),
+        ("s02", "reverse"),
+        ("p01", "reverse"),
+        ("s01", "reverse"),
+        ("p01", "reverse"),
+        ("s01", "reverse")]
+    
