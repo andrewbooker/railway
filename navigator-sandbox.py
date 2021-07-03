73fda03be7b7950374a481e890dd1e33a5bea8bf
Andrew Booker <improvizone@gmail.com>
2021-07-03 17:12:02 +0100

    changing direction at a dead end and indicating required points setting

--- navigator-sandbox.py
+++ navigator-sandbox.py
@@ -10,6 +10,7 @@ class Journey():
     def __init__(self, layout):
         self.layout = layout
         self.direction = "forward"
+        self.history = []
         self._at(self.layout["sections"][0])
 
     def _find(self, id):
@@ -24,22 +25,32 @@ class Journey():
 
     def _at(self, s):
         self.section = s
+        self.history.append(s["id"])
         print("now on", s["name"])
+
+    def _changeDirection(self):
+        self.direction = "forward" if self.direction == "reverse" else "reverse"
 
     def nextStage(self):
         print("from", self.section["name"])
         if "next" not in self.section:
             if self.section["id"][0] == "p":
-                choice = "left_id" if (randint(0, 1) > 0) else "right_id"
-                print("choosing", choice.split("_")[0])
-                self._at(self._find(self.section[choice]))
+                if self.direction == "forward":
+                    choice = "%s_id" % self.section["param"] if "param" in self.section else ("left_id" if (randint(0, 1) > 0) else "right_id")
+                    print("choosing", choice.split("_")[0])
+                    self._at(self._find(self.section[choice]))
+                else:
+                    print("points expected to be", "left" if self.section["left_id"] == self.history[-2] else "right")
+                    for s in self.layout["sections"]:
+                        if "forward" in s["next"] and s["next"]["forward"]["id"] == self.section["id"]:
+                            self._at(s)
         else:
             options = self.section["next"]
             if self.direction in options:
                 self._at(self._find(options[self.direction]["id"]))
             else:
-                print("the end")
-                exit()
+                self._changeDirection()
+                print("changing direction to", self.direction)
 
 layout = None
 with open(sys.argv[1], "r") as layoutSpec:
@@ -57,4 +68,5 @@ while not shouldStop.is_set():
         journey.nextStage()
     if c == "q":
         shouldStop.set()
+        print(journey.history)
         exit()
