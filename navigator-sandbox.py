9ee59ed902511d6d0dd2834d6d7fae62bf107287
Andrew Booker <improvizone@gmail.com>
2021-07-03 16:10:44 +0100

    navigating forwards through a layout with one set of points and a dead end

--- navigator-sandbox.py
+++ navigator-sandbox.py
@@ -3,30 +3,50 @@
 import json
 import threading
 import readchar
+import sys
+from random import randint
 
 class Journey():
     def __init__(self, layout):
         self.layout = layout
         self.direction = "forward"
-        self.section = self.layout["sections"][0]
+        self._at(self.layout["sections"][0])
 
-    def _find(self, option):
+    def _find(self, id):
+        if "points" in self.layout:
+            for p in self.layout["points"]:
+                if p["id"] == id:
+                    return p
         for s in self.layout["sections"]:
-            if s["id"] == option["id"]:
+            if s["id"] == id:
                 return s
         return None
 
+    def _at(self, s):
+        self.section = s
+        print("now on", s["name"])
+
     def nextStage(self):
-        options = self.section["next"]
-        if self.direction in options:
-            self.section = self._find(options[self.direction])
-            print(self.section["name"])
+        print("from", self.section["name"])
+        if "next" not in self.section:
+            if self.section["id"][0] == "p":
+                choice = "left_id" if (randint(0, 1) > 0) else "right_id"
+                print("choosing", choice.split("_")[0])
+                self._at(self._find(self.section[choice]))
+        else:
+            options = self.section["next"]
+            if self.direction in options:
+                self._at(self._find(options[self.direction]["id"]))
+            else:
+                print("the end")
+                exit()
 
 layout = None
-with open("example-layouts/single-loop.json", "r") as layoutSpec:
+with open(sys.argv[1], "r") as layoutSpec:
     layout = json.loads(layoutSpec.read())
 
-print(len(layout["sections"]), "sections")
+print("sections:", len(layout["sections"]))
+print("points:", len(layout["points"]) if "points" in layout else 0)
 
 journey = Journey(layout)
 
