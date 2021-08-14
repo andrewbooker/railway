ab7bb28e2e036b5390f2dbdeb7eb1de520cac8f1
Andrew Booker <improvizone@googlemail.com>
2021-08-14 15:19:30 +0100

    demoing a one-section layout with a detector at each end

--- run.py
+++ run.py
@@ -8,10 +8,11 @@ import threading
 def say(what):
     sys.stdout.write("%s\r\n" % what)
 
-
 class RoutingController():
-    def __init__(self, instructionProvider):
+    def __init__(self, instructionProvider, layout):
         self.instructions = []
+        self.currentInstruction = None
+        self.layout = layout
         self.instructionProvider = instructionProvider
         self.checkpoints = {}
         self.sections = {}
@@ -22,8 +23,15 @@ class RoutingController():
             return
 
         ins = self.instructions[0]
-        print("attempting", ins.describe())
+        if self.currentInstruction is not None:                
+            sectionLayout = self.layout[ins.name] # so far only have section instructions
+            until = sectionLayout["until"]
+            if until is None or until[ins.direction] is None or until[ins.direction].state() == 0:
+                return
+            
+        print("attempting", ins.describe()) # this will issue commands to the motion controller or change points
 
+        self.currentInstruction = ins
         self.instructions.pop(0)
 
     def start(self, shouldStop):
@@ -38,10 +46,10 @@ import time
 class SectionInstruction():
     def __init__(self, name, dir):
         self.name = name
-        self.section = dir
+        self.direction = dir
 
     def describe(self):
-        return "%s %s" % (self.name, self.section)
+        return "%s %s" % (self.name, self.direction)
 
 class ShuttleOnSectionA():
     def __init__(self):
@@ -54,11 +62,37 @@ class ShuttleOnSectionA():
         else:
             return SectionInstruction("A", "reverse")
 
+class Detector():
+    def state(self):
+        return 0
+        
+class TimedSimulatingDetector():
+    def __init__(self, t):
+        self.dt = t
+        self.last = time.time()
 
+    def state(self):
+        now = time.time()
+        if (now - self.last) > self.dt:
+            self.last = now
+            return 1
+        return 0
+
+aStart = TimedSimulatingDetector(3)
+aEnd = TimedSimulatingDetector(2)
+
+layout = {
+    "A": {
+        "until": {
+            "forwards": aStart,
+            "reverse": aEnd
+        }
+    }
+}
 
 print("starting")
 shuttle = ShuttleOnSectionA()
-routingCtrl = RoutingController(shuttle)
+routingCtrl = RoutingController(shuttle, layout)
 
 threadables = [
     Cmd(say),
