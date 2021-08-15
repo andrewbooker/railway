e95c5112848ad1b81031a9483af0ceb38a85f872
Andrew Booker <improvizone@gmail.com>
2021-08-15 19:50:25 +0100

    moving servo sandbox into sandbox directory

--- sandbox/servo.py
+++ sandbox/servo.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 
 
+import sys
+import os
+parentDir = os.path.dirname(os.getcwd())
+if "railway" not in parentDir:
+    print("needs to run in sandbox")
+    exit()
+sys.path.append(parentDir)
 from lib.points import Points
+from lib.rpiPorts import UsingRPi
 import time
 import random
 
-from lib.rpiPorts import UsingRPi
-
 rpi = UsingRPi()
-
 points = Points(24)
 try:
     while True:
