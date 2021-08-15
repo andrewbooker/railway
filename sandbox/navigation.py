8bcf55b2ec68dcb62c15fb829dc095aaddd7ee82
Andrew Booker <improvizone@gmail.com>
2021-08-15 10:35:18 +0100

    moving navigator test into sandbox folder

--- sandbox/navigation.py
+++ sandbox/navigation.py
@@ -4,6 +4,13 @@
 import threading
 import readchar
 import sys
+import os
+
+parentDir = os.path.dirname(os.getcwd())
+if "railway" not in parentDir:
+    print("needs to run in sandbox")
+    exit()
+sys.path.append(parentDir)
 from lib.navigator import Journey
 
 class Reporter():
