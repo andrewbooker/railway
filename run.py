d5e9ea986864f90e274012307aee7c73710dfa4c
Andrew Booker <improvizone@gmail.com>
2021-08-21 14:04:25 +0100

    trying out another navigation idea on the simple shuttle track driven by detectors simulated with keystrokes

---
+++ run.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+
+
+import threading
+import readchar
+import sys
+import time
+from lib.navigator import Journey
+
+
+layoutStr = None
+if len(sys.argv) < 2:
+    print("specify layout json file")
+    exit()
+
+with open(sys.argv[1], "r") as layoutSpec:
+    layoutStr = layoutSpec.read()
+
+def say(*what):
+    p = what[0]
+    for i in range(1, len(what)):
+        p = "%s %s" % (p, what[i])
+    sys.stdout.write("%s\r\n" % p)
+
+class NavigationListener():
+    def __init__(self, detectionListener):
+        self.detectionListener = detectionListener
+        self.currentDirection = "forward"
+        self.currentSection = None
+
+    def setDetector(self):
+        if "until" in self.currentSection:
+            u = self.currentSection["until"]
+            if self.currentDirection in u:
+                d = u[self.currentDirection]
+                self.detectionListener.setNextDetector("%s_%s" % (d["bank"], d["port"]), 1)
+
+    def changeDirection(self, to):
+        say("changing direction to", to)
+        self.currentDirection = to
+        self.setDetector()
+
+    def moveTo(self, section):
+        say("setting section to", section["name"])
+        self.currentSection = section
+        self.setDetector()
+
+    def setPointsTo(self, s, p):
+        #detectionListener.setNextDetector...
+        say("setting points", p["name"], "to", s)
+
+    def waitToSetPointsTo(self, s, p):
+        say("setting points", p["name"], "to", s, "if ready")
+
+
+class TrafficListener():
+    def __init__(self, detectors):
+        self.detectors = detectors
+        self.callback = None
+        self.detector = None
+        self.requiredState = None
+
+    def setCallback(self, c):
+        self.callback = c
+
+    def setNextDetector(self, d, state):
+        self.detector = d
+        self.requiredState = state
+        say("waiting for", state, "on", d)
+
+    def poll(self):
+        if self.detector is None or self.callback is None:
+            return
+        if self.detectors.stateOf(self.detector) == self.requiredState:
+            self.callback()
+
+class Detector():
+    def __init__(self):
+        self.state = 0
+
+class KeyboardDetectors():
+    def __init__(self):
+        self.lookup = {
+            "RPi_14": "1",
+            "RPi_15": "2"
+        }
+        self.detectors = {}
+
+    def stateOf(self, of):
+        if of not in self.lookup:
+            return 0
+        c = self.lookup[of]
+        if c not in self.detectors:
+            return 0
+        return self.detectors[c].state
+
+    def onCmd(self, c):
+        if c not in self.detectors:
+            self.detectors[c] = Detector()
+        self.detectors[c].state = 0 if self.detectors[c].state == 1 else 1
+
+
+detectors = KeyboardDetectors()
+traffic = TrafficListener(detectors)
+navigation = NavigationListener(traffic)
+journey = Journey(layoutStr, navigation)
+traffic.setCallback(journey.nextStage)
+
+
+class ControlLoop():
+    def __init__(self, c, i):
+        self.c = c
+        self.i = i
+
+    def start(self, shouldStop):
+        while not shouldStop.is_set():
+            self.c()
+            time.sleep(self.i)
+
+controlLoop = ControlLoop(traffic.poll, 1.0)
+
+from lib.cmd import Cmd, shouldStop
+
+print("starting")
+
+cmd = Cmd(detectors.onCmd)
+threadables = [
+    cmd,
+    controlLoop
+]
+threads = [threading.Thread(target=t.start, args=(shouldStop,), daemon=True) for t in threadables]
+[thread.start() for thread in threads]
+[thread.join() for thread in threads]
+
+#del ports
+print("stopped")
