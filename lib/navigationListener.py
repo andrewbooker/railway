6062bfb7e8fe1628fe18d6d370481d55fd6b91b3
Andrew Booker <improvizone@gmail.com>
2021-08-22 22:46:18 +0100

    pulling navigation listener into its own file

---
+++ lib/navigationListener.py
@@ -0,0 +1,58 @@
+
+
+class NavigationListener():
+    def __init__(self, detectionListener, directionController, pointsController):
+        self.detectionListener = detectionListener
+        self.directionController = directionController
+        self.pointsController = pointsController
+        self.currentDirection = "forward"
+        self.currentSection = None
+        self.nextRequestor = None
+
+    @staticmethod
+    def portId(p):
+        return "%s_%s" % (p["bank"], p["port"])
+
+    def _set(self):
+        self.detectionListener.clearCallback()
+        self.directionController.set(NavigationListener.portId(self.currentSection["direction"]), self.currentDirection)
+
+        if self.currentSection["id"][0] == "p":
+            self.nextRequestor()
+            return
+
+        if "until" in self.currentSection:
+            u = self.currentSection["until"]
+            if self.currentDirection in u:
+                d = u[self.currentDirection]
+                self.detectionListener.setCallback(self.nextRequestor)
+                self.detectionListener.setNextDetector(NavigationListener.portId(d), 1)
+
+        if "next" in self.currentSection:
+            n = self.currentSection["next"]
+            if self.currentDirection in n and n[self.currentDirection]["id"][0] == "p":
+                points = self.pointsController.fromId(n[self.currentDirection]["id"])
+                self.detectionListener.setCallback(self.nextRequestor)
+                self.detectionListener.setNextDetector(NavigationListener.portId(points["detector"]), 0)
+
+
+    def setNextRequestor(self, r):
+        self.nextRequestor = r
+
+    def changeDirection(self, to):
+        self.currentDirection = to
+        if "until" in self.currentSection:
+            self._set()
+
+    def moveTo(self, section):
+        self.currentSection = section
+        self._set()
+
+    def setPointsTo(self, s, p):
+        self.detectionListener.waitFor(NavigationListener.portId(p["detector"]), 1)
+        self.pointsController.set(p["id"], s)
+
+    def waitToSetPointsTo(self, s, p):
+        self.detectionListener.setCallback(self.nextRequestor)
+        self.detectionListener.setNextDetector(NavigationListener.portId(p["detector"]), 0)
+        self.pointsController.set(p["id"], s)
--- run.py
+++ run.py
@@ -22,66 +22,8 @@ def say(*what):
         p = "%s %s" % (p, what[i])
     sys.stdout.write("%s\r\n" % p)
 
-class NavigationListener():
-    def __init__(self, detectionListener, directionController, pointsController):
-        self.detectionListener = detectionListener
-        self.directionController = directionController
-        self.pointsController = pointsController
-        self.currentDirection = "forward"
-        self.currentSection = None
-        self.nextRequestor = None
 
-    @staticmethod
-    def portId(p):
-        return "%s_%s" % (p["bank"], p["port"])
-
-    def _set(self):
-        self.detectionListener.clearCallback()
-        self.directionController.set(NavigationListener.portId(self.currentSection["direction"]), self.currentDirection)
-
-        if self.currentSection["id"][0] == "p":
-            say("now at points", self.currentSection["name"])
-            self.nextRequestor()
-            return
-
-        if "until" in self.currentSection:
-            u = self.currentSection["until"]
-            if self.currentDirection in u:
-                d = u[self.currentDirection]
-                self.detectionListener.setCallback(self.nextRequestor)
-                self.detectionListener.setNextDetector(NavigationListener.portId(d), 1)
-
-        if "next" in self.currentSection:
-            n = self.currentSection["next"]
-            if self.currentDirection in n and n[self.currentDirection]["id"][0] == "p":
-                points = pointsController.fromId(n[self.currentDirection]["id"])
-                say("next will be points", points["name"])
-                self.detectionListener.setCallback(self.nextRequestor)
-                self.detectionListener.setNextDetector(NavigationListener.portId(points["detector"]), 0)
-
-
-    def setNextRequestor(self, r):
-        self.nextRequestor = r
-
-    def changeDirection(self, to):
-        say("changing direction to", to)
-        self.currentDirection = to
-        if "until" in self.currentSection:
-            self._set()
-
-    def moveTo(self, section):
-        say("setting section to", section["name"])
-        self.currentSection = section
-        self._set()
-
-    def setPointsTo(self, s, p):
-        self.detectionListener.waitFor(NavigationListener.portId(p["detector"]), 1)
-        self.pointsController.set(p["id"], s)
-
-    def waitToSetPointsTo(self, s, p):
-        self.detectionListener.setCallback(self.nextRequestor)
-        self.detectionListener.setNextDetector(NavigationListener.portId(p["detector"]), 0)
-        self.pointsController.set(p["id"], s)
+from lib.navigationListener import NavigationListener
 
 
 class TrafficListener():
