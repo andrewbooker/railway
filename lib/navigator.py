ba8cf2a8db0d0fb08ffc8c1e2a6f5164874a2e53
Andrew Booker <improvizone@gmail.com>
2021-09-01 18:41:46 +0100

    reducing points code duplication

--- lib/navigator.py
+++ lib/navigator.py
@@ -43,6 +43,14 @@ class Journey():
             if Journey._check(next, atPointsId, "reverse") is not None:
                 return (s, "reverse", None)
 
+    def _approachDivergence(self, points, stage):
+        choice = self.selectPoints()
+        self.listener.setPointsTo(choice, stage, points)
+        nextDirection = points[stage][choice]["direction"] if "direction" in points[stage][choice] else "forward"
+        if nextDirection != self.direction:
+            self.changeDirection()
+        self._at(self._find(points[stage][choice]["id"]))
+
     def start(self):
         self._at(self.layout[0])
 
@@ -58,29 +66,17 @@ class Journey():
                 approachingDivergence = len(previousSection["next"][self.direction]["params"]) < 2
                 stage = previousSection["next"][self.direction]["params"][0] if len(previousSection["next"][self.direction]["params"]) > 0 else "outgoing"
                 if approachingDivergence:
-                    choice = self.selectPoints()
-                    self.listener.setPointsTo(choice, stage, points)
-                    nextDirection = points[stage][choice]["direction"] if "direction" in points[stage][choice] else "forward"
-                    if nextDirection != self.direction:
-                        self.changeDirection()
-                    self._at(self._find(points[stage][choice]["id"]))
+                    self._approachDivergence(points, stage)
                 else:
                     expectedPoints = previousSection["next"][self.direction]["params"][1]
                     self.listener.waitToSetPointsTo(expectedPoints, stage, points)
                     (nextSection, nextDirection, nextPointsStage) = self._afterConvergence(points["id"], stage)
                     if nextPointsStage is not None:
-                        #copied from above, changed stage to nextPointsStage:
-                        choice = self.selectPoints()
-                        self.listener.setPointsTo(choice, nextPointsStage, points)
-                        nextDirection = points[nextPointsStage][choice]["direction"] if "direction" in points[nextPointsStage][choice] else "forward"
-                        if nextDirection != self.direction:
-                            self.changeDirection()
-                        self._at(self._find(points[nextPointsStage][choice]["id"]))
+                        self._approachDivergence(points, nextPointsStage)
                     else:
                         if nextDirection == self.direction:
                             self.changeDirection()
                         self._at(nextSection)
-
         else:
             options = self.section["next"]
             if self.direction in options:
