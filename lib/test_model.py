153b8b2d40e92d55dcbd302062f98ecdbea37af9
Andrew Booker <improvizone@gmail.com>
2021-09-08 22:58:19 +0100

    including hardware details read from example file

--- lib/test_model.py
+++ lib/test_model.py
@@ -7,6 +7,9 @@ class Section():
         self.name = name
         self.next = None
         self.previous = None
+        self.direction = None
+        self.forwardUntil = None
+        self.reverseUntil = None
 
 class Model():
     def __init__(self, m):
@@ -21,26 +24,35 @@ class Model():
                     section.next = (n["forward"]["id"], "forward")
                 if "reverse" in n:
                     section.previous = (n["forward"]["id"], "reverse")
+            if "direction" in s: # should not be optional, no section of track can be unpowered
+                d = s["direction"]
+                section.direction = (d["bank"], d["port"])
+            if "until" in s:
+                u = s["until"]
+                if "forward" in u:
+                    uf = u["forward"]
+                    section.forwardUntil = (uf["bank"], uf["port"])
+                if "reverse" in u:
+                    uf = u["reverse"]
+                    section.reverseUntil = (uf["bank"], uf["port"])
             self.sections[s["id"]] = section
 
 
-straightLine = """
-[
-    {
-        "id": "s01",
-        "name": "shuttle", 
-        "next": {}
-    }
-]
-"""
+def openLayout(fileName):
+    with open(fileName, "r") as layoutSpec:
+        return layoutSpec.read()
+
 
 def test_single_section():
-    m = Model(straightLine)
+    m = Model(openLayout("example-layouts/shuttle.json"))
 
     section = m.sections["s01"]
-    assert section.name == "shuttle"
+    assert section.name == "shuttle track"
     assert section.next == None
     assert section.previous == None
+    assert section.direction == ("RPi", 23)
+    assert section.forwardUntil == ("RPi", 14)
+    assert section.reverseUntil == ("RPi", 15)
 
 loop = """
 [
@@ -55,10 +67,14 @@ loop = """
 ]
 """
 
-def test_loop_has_self_referencing_next_and_previous():
+def test_loop_has_self_referencing_next_and_previous_with_required_directions():
     m = Model(loop)
 
     section = m.sections["s01"]
     assert section.name == "loop"
     assert section.next == ("s01", "forward")
     assert section.previous == ("s01", "reverse")
+
+
+
+
