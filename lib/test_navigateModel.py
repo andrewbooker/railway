3797fdaffb545b8cb2b8a3949a799f7376ffcc20
Andrew Booker <improvizone@gmail.com>
2021-09-15 22:24:29 +0100

    fixing current issues

--- lib/test_navigateModel.py
+++ lib/test_navigateModel.py
@@ -24,37 +24,67 @@ class NavigateModel():
         self.model = model
         self.listener = listener
         self.pointsSelection = lambda: "left"
-        self.pointsCourse = None
-        for s in model.sections:
-            self.occupy = (s, "forward")
-            return
+        self.initialDir = "forward"
+        self.occupy = []
+
 
     def initialDirection(self, d):
-        self.occupy = (self.occupy[0], d)
+        self.initialDir = d
 
     @staticmethod
     def opposite(d):
         return "reverse" if d == "forward" else "forward"
 
     def next(self):
-        (sectionId, direction) = self.occupy
+        if len(self.occupy) == 0:
+            for s in self.model.sections:
+                self.occupy.append((s, self.initialDir))
+                break
+        current = self.occupy[-1]
+        (sectionId, direction) = current[:2]
+        approachingConvergence = len(current) > 2
+        currentStage = current[2] if approachingConvergence else "outgoing"
+        currentSelection = current[3] if approachingConvergence else None
         section = self.model.sections[sectionId]
+        isPoints = section.__class__ == Points
 
-        if (direction == "forward" and section.next is None) or (direction == "reverse" and section.previous is None):
-            self.occupy = (sectionId, NavigateModel.opposite(direction))
-        elif direction == "forward" and section.next is not None:
-            self.occupy = section.next
-        elif direction == "reverse" and section.previous is not None:
-            self.occupy = section.previous
-        self.listener.connect({"id": sectionId}, direction)
+        nextSection = None
 
-        if section.__class__ == Points:
-            if self.pointsCourse is None and direction == "forward":
-                self.listener.setPointsTo(self.pointsSelection(), "outgoing", section)
-                self.pointsCourse = section.outgoing.left
-            if self.pointsCourse is not None and direction == "reverse":
-                self.listener.waitToSetPointsTo(self.pointsSelection(), "outgoing", section)
-                self.pointsCourse = None
+        if direction == "forward" and section.next is not None:
+            nextSection = section.next
+        if direction == "reverse" and section.previous is not None:
+            nextSection = section.previous
+        if not isPoints and ((direction == "forward" and section.next is None) or (direction == "reverse" and section.previous is None)):
+            nextSection = (sectionId, NavigateModel.opposite(direction))
+        if (direction == "forward" and section.forwardUntil is not None) or (direction == "reverse" and section.reverseUntil is not None):
+            nextSection = (sectionId, NavigateModel.opposite(direction))
+
+        if nextSection is not None:
+            self.occupy.append(nextSection)
+
+        if not isPoints or (not approachingConvergence and (currentStage == "outgoing" and direction == "reverse") or (currentStage == "incoming" and direction == "forward")):
+            self.listener.connect({"id": sectionId}, direction)
+            return
+
+        if approachingConvergence:
+            self.listener.waitToSetPointsTo(currentSelection, currentStage, section)
+            self.listener.connect({"id": sectionId}, direction)
+        else:
+            selection = self.pointsSelection()
+            stage = getattr(section, currentStage)
+            course = getattr(stage, selection)
+            self.listener.connect({"id": sectionId}, direction)
+            self.listener.setPointsTo(selection, currentStage, section)
+            if direction == "forward":
+                if course.next is not None:
+                    self.occupy.append(course.next)
+                elif course.forwardUntil is not None:
+                    self.occupy.append((sectionId, NavigateModel.opposite(direction), currentStage, selection))
+            if direction == "reverse":
+                if course.previous is not None:
+                    self.occupy.append(course.previous)
+                elif course.reverseUntil is not None:
+                    self.occupy.append((sectionId, NavigateModel.opposite(direction), currentStage, selection))
 
 def test_shuttle():
     m = Model(openLayout("example-layouts/shuttle.json"))
@@ -121,6 +151,7 @@ def test_two_stage_loop_reverse():
     nm.next()
     assert listener.history == [("s01", "reverse"), ("s02", "reverse"), ("s01", "reverse")]
 
+
 def test_self_contained_points_forward_left():
     m = Model(openLayout("example-layouts/points-as-large-y.json"))
     listener = ReportingListener()
@@ -136,18 +167,19 @@ def test_self_contained_points_forward_left():
     assert listener.history == [
         ("p01", "forward"),
         ("outgoing points selection", "left"),
-        ("p01", "reverse"),
-        ("outgoing points condition", "left")
+        ("outgoing points condition", "left"),
+        ("p01", "reverse")
     ]
     nm.next()
     assert listener.history == [
         ("p01", "forward"),
         ("outgoing points selection", "left"),
-        ("p01", "reverse"),
         ("outgoing points condition", "left"),
+        ("p01", "reverse"),
         ("p01", "forward"),
         ("outgoing points selection", "left")
     ]
+
 
 def test_self_contained_points_reverse_right():
     m = Model(openLayout("example-layouts/points-as-large-y.json"))
@@ -171,16 +203,59 @@ def test_self_contained_points_reverse_right():
         ("p01", "reverse"),
         ("p01", "forward"),
         ("outgoing points selection", "right"),
-        ("p01", "reverse"),
-        ("outgoing points condition", "right")
+        ("outgoing points condition", "right"),
+        ("p01", "reverse")
     ]
     nm.next()
     assert listener.history == [
         ("p01", "reverse"),
         ("p01", "forward"),
         ("outgoing points selection", "right"),
-        ("p01", "reverse"),
         ("outgoing points condition", "right"),
+        ("p01", "reverse"),
         ("p01", "forward"),
         ("outgoing points selection", "right")
+    ]
+
+
+def test_simple_fork_points_left():
+    m = Model(openLayout("example-layouts/simple-fork.json"))
+    listener = ReportingListener()
+    nm = NavigateModel(m, listener)
+    nm.pointsSelection = lambda: "left"
+
+    nm.next()
+    assert listener.history == [
+        ("s01", "forward")
+    ]
+    nm.next()
+    assert listener.history == [
+        ("s01", "forward"),
+        ("p01", "forward"),
+        ("outgoing points selection", "left")
+    ]
+    nm.next()
+    assert listener.history == [
+        ("s01", "forward"),
+        ("p01", "forward"),
+        ("outgoing points selection", "left"),
+        ("s02", "forward")
+    ]
+    nm.next()
+    assert listener.history == [
+        ("s01", "forward"),
+        ("p01", "forward"),
+        ("outgoing points selection", "left"),
+        ("s02", "forward"),
+        ("s02", "reverse")
+    ]
+    nm.next()
+    assert listener.history == [
+        ("s01", "forward"),
+        ("p01", "forward"),
+        ("outgoing points selection", "left"),
+        ("s02", "forward"),
+        ("s02", "reverse"),
+        ("outgoing points condition", "left"),
+        ("p01", "reverse")
     ]
