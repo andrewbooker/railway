b20b632b6d9cc93c31d9b42857340f0e612ec526
Andrew Booker <improvizone@gmail.com>
2021-12-12 18:30:15 +0000

    swapping temporary arduino controller for the four sections of track currently wired

--- controller.py
+++ controller.py
@@ -9,17 +9,34 @@ print("")
 portA = 12
 portB = 18
 
-from lib.rpiPorts import PwmPort, Output, UsingRPi
+from lib.rpiPorts import UsingRPi
+from lib.arduinoPorts import UsingArduino
 rpi = UsingRPi()
+ard = UsingArduino()
+
+class TmpArd():
+    def __init__(self):
+        self.a = [
+            ard.output(48),
+            ard.output(49),
+            ard.output(50),
+            ard.output(51)
+        ]
+
+    def set(self, v):
+        for a in self.a:
+            a.set(v)
+
 
 from lib.monitor import PowerMonitor
 from lib.speed import MotionController, Speed
 from lib.distribution import Direction
 from lib.detectors import Detector
 
+pwr = TmpArd()
 monitor = PowerMonitor()
 speed = Speed(rpi.pwmPort(12), monitor)
-direction = Direction(rpi.output(23))
+direction = Direction(pwr)
 
 controller = MotionController(speed, {"any": direction}, monitor, 70, "any")
 detectorA = Detector(rpi.input(14), "A", controller.onPass)
@@ -27,14 +44,14 @@ detectorB = Detector(rpi.input(15), "B", controller.onPass)
 
 from lib.cmd import *
 cmd = Cmd(controller.onCmd)
-controlLoop = ControlLoop(lambda: controller.onCmd('d'), 64)
+#controlLoop = ControlLoop(lambda: controller.onCmd('d'), 64)
 
 targets = [
     speed,
     detectorA,
     detectorB,
     cmd,
-    controlLoop
+    #controlLoop
 ]
 threads = [threading.Thread(target=t.start, args=(shouldStop,), daemon=True) for t in targets]
 
