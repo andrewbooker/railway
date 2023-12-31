2c82fd5583bfbbb863923493d19aedf1cd400028
Andrew Booker <improvizone@gmail.com>
2023-12-31 12:34:10 +0000

    simplifying controller

--- controller.py
+++ controller.py
@@ -1,43 +1,44 @@
 #!/usr/bin/env python
 
-import os
-
-from application.directionRelays import DirectionRelays
 from lib.monitor import PowerMonitor
 from lib.speed import Speed
 from application.commandBasedMotionController import CommandBasedMotionController
-from lib.distribution import Direction
 from lib.detectors import Detector
+from lib.routeNavigator import DirectionController
 from lib.rpiPorts import UsingRPi
 from lib.arduinoPorts import UsingArduino
-os.system("clear")
-
-portA = 12
-portB = 18
+from lib.cmd import *
 
 rpi = UsingRPi()
 ard = UsingArduino()
-
 monitor = PowerMonitor()
 speed = Speed(rpi.pwmPort(12), monitor)
+
 
 def onPass(a, b):
     monitor.setMessage("points %s %s" % (a, b))
 
-directionController = DirectionRelays(ard, monitor.msg)
-directionController.portId = "arduino_41"
+
+class UniversalDirectionController(DirectionController):
+    def __init__(self, device: UsingArduino):
+        DirectionController.__init__(self)
+        self.ports = {p: device.output(p) for p in [41, 48, 50, 51]}  # can read these direct out of Model from layout.json
+
+    def set(self, portId, direction):
+        DirectionController.set(self, portId, direction)
+        for p in self.ports:
+            self.ports[p].set(0 if direction == "forwards" else 1)
+
+
+directionController = UniversalDirectionController(ard)
 controller = CommandBasedMotionController(speed, monitor.msg, 90, directionController)
 
-detectorA = Detector(rpi.input(14), "A", onPass)
-detectorB = Detector(rpi.input(15), "B", onPass)
-
-from lib.cmd import *
 cmd = Cmd(controller.onCmd)
 
 targets = [
     speed,
-    detectorA,
-    detectorB,
+    Detector(rpi.input(14), "A", onPass),
+    Detector(rpi.input(15), "B", onPass),
     cmd
 ]
 threads = [threading.Thread(target=t.start, args=(shouldStop,), daemon=True) for t in targets]
@@ -45,5 +46,5 @@ threads = [threading.Thread(target=t.start, args=(shouldStop,), daemon=True) for
 [thread.start() for thread in threads]
 [thread.join() for thread in threads]
 
-
 del rpi
+del ard
