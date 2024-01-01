0df5e4879508b994c17c863c35047035de6f292c
Andrew Booker <improvizone@gmail.com>
2024-01-01 13:52:13 +0000

    fixing direction relays under new Direction enum

--- application/directionRelays.py
+++ application/directionRelays.py
@@ -1,6 +1,6 @@
 from lib.monitor import StatusComponent
 from lib.ports import Ports
-from lib.directionController import DirectionController
+from lib.directionController import DirectionController, Direction
 
 
 class DirectionRelays(DirectionController):
@@ -10,11 +10,11 @@ class DirectionRelays(DirectionController):
         self.status = status
         self.ports = {}
 
-    def set(self, portId, direction):
+    def set(self, portId, direction: Direction):
         DirectionController.set(self, portId, direction)
         if portId not in self.ports:
             bank, i = tuple(portId.split("_"))
             self.status.setValue(f"finding output {i} in {bank}")
             self.ports[portId] = self.device.output(int(i))
-        self.status.setValue(f"setting {portId} relay to {direction}")
-        self.ports[portId].set(0 if direction == "forwards" else 1)
+        self.status.setValue(f"setting {portId} relay to {direction.value}")
+        self.ports[portId].set(0 if direction == Direction.Forward else 1)
