a134f7360a8c984c8f48aa07b9a0ec4e106b36c5
Andrew Booker <improvizone@gmail.com>
2024-03-03 15:34:36 +0000

    correcting RPi port number in detector test

---
+++ sandbox/detectors.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import os
+import sys
+parentDir = os.path.dirname(os.getcwd())
+if "railway" not in parentDir:
+    print("needs to run in sandbox")
+    exit()
+sys.path.append(parentDir)
+from lib.rpiPorts import UsingRPi
+from lib.cmd import *
+from lib.detectors import Detector
+from stdump import Dump
+
+
+possiblePorts = [
+    8, 14, 15, 21, 24
+]
+
+rpi = UsingRPi()
+def doNothing(t):
+    pass
+
+cmd = Cmd(doNothing)
+
+threadables = [
+    cmd
+]
+
+
+def onPass(a, b):
+    Dump().write("%s %s" % (a, b))
+
+for p in possiblePorts:
+    threadables.append(Detector(rpi.input(p), "port %d" % p, onPass))
+
+threads = [threading.Thread(target=t.start, args=(shouldStop,), daemon=True) for t in threadables]
+[thread.start() for thread in threads]
+[thread.join() for thread in threads]
+
+print("stopped")
