5e9093e14ac707ebb24edaa00523a166c78e42d9
Andrew Booker <improvizone@gmail.com>
2024-04-06 12:26:12 +0100

    getting detector tester running (but not working)

--- sandbox/detectors.py
+++ sandbox/detectors.py
@@ -9,33 +9,42 @@ if "railway" not in parentDir:
 sys.path.append(parentDir)
 from lib.rpiPorts import UsingRPi
 from lib.cmd import *
-from lib.detectors import Detector
-from stdump import Dump
 
 
-possiblePorts = [
-    8, 14, 15, 21, 24
-]
+def say(txt):
+    sys.stdout.write(f"{txt}\r\n")
+    sys.stdout.flush()
 
-rpi = UsingRPi()
+
+class Detector:
+    def __init__(self, port, pos, callback):
+        self.callback = callback
+        self.pos = pos
+        self.port = port
+        self.state = None
+
+    def start(self, shouldStop):
+        while not shouldStop.is_set():
+            v = self.port.get()
+            if v != self.state:
+                self.callback(v, self.pos)
+                self.state = v
+            time.sleep(0.05)
+
 def doNothing(t):
     pass
 
-cmd = Cmd(doNothing)
 
-threadables = [
-    cmd
-]
+rpi = UsingRPi()
+threadables = [Cmd(doNothing)]
 
-
-def onPass(a, b):
-    Dump().write("%s %s" % (a, b))
-
-for p in possiblePorts:
-    threadables.append(Detector(rpi.input(p), "port %d" % p, onPass))
+for p in [8, 14, 15, 21, 24]:
+    say(f"registering RPi port {p}")
+    threadables.append(Detector(rpi.input(p), f"port {p}", lambda a, b: say(f"{a} {b}")))
 
 threads = [threading.Thread(target=t.start, args=(shouldStop,), daemon=True) for t in threadables]
+say("starting")
 [thread.start() for thread in threads]
+say("started")
 [thread.join() for thread in threads]
-
-print("stopped")
+say("stopped")
